5776cc8123c21a954550c9d61241bcb4ca197628
Muhammad-Talha-Naeem <75784944+Muhammad-Talha-Naeem@users.noreply.github.com>
2021-09-01 10:40:48 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <TaskAxios/>
+</template>
+
+<script>
+import TaskAxios from './components/task-axios.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TaskAxios
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/task-axios.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <table>
+            <tr v-for="product in products" v-bind:key="product.name">
+                <td><img v-bind:src="product.imgUrl" width="200"  v-bind:alt="product.name"></td>
+                <td>{{product.name}}</td>
+                <td>{{product.price}}</td>
+            </tr>
+        </table>
+        <h1>hy</h1>
+    </div>
+</template>
+<script>
+import Axios from "axios";
+export default {
+    name : "Task",
+    data(){
+        return{
+            products:[],
+        }
+    },
+    mounted(){
+        Axios.get('http://localhost:3000/products')
+        .then(resp=> this.products = resp.data);
+
+    }
+}
+</script>
